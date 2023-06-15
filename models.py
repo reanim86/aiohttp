0363c406e76a391b34eab373c232a58bf6172635
Aleksei Kudinov <reanim86@yandex.ru>
2023-06-15 16:39:33 +0300

    Initial commit

---
+++ models.py
@@ -0,0 +1,32 @@
+from sqlalchemy import Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, relationship
+
+PG_DSN = 'postgresql+asyncpg://postgres:Tehn89tehn@127.0.0.1:5431/ads'
+engine = create_async_engine(PG_DSN)
+Session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
+
+class User(Base):
+    """
+    Таблица пользователей
+    """
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(length=40), nullable=True, unique=True, index=True)
+    password = Column(String, nullable=True)
+    creationdate = Column(DateTime, server_default=func.now())
+class AdsTable(Base):
+    """
+    Таблица объявлений
+    """
+    __tablename__ = 'adstable'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    head = Column(String(length=100), nullable=True, index=True)
+    description = Column(String(length=500))
+    creationdate = Column(DateTime, server_default=func.now())
+    username = Column(Integer, ForeignKey('user.id'), nullable=True)
+
+    user = relationship(User, backref='adstable')
---
+++ schema.py
@@ -0,0 +1,61 @@
+import pydantic
+from typing import Optional
+
+class CreateAds(pydantic.BaseModel):
+    """
+    Валидация полей при создании записи в модели AdsTable
+    """
+    head: str
+    description: Optional[str]
+    username: str
+    password: str
+class PatchAds(pydantic.BaseModel):
+    """
+    Валидация полей при редактировании записи в модели AdsTable
+    """
+    head: Optional[str]
+    description: Optional[str]
+    username: Optional[str]
+    password: Optional[str]
+
+class CreateUser(pydantic.BaseModel):
+    """
+     Валидация полей при создании записи в модели User
+    """
+    email: str
+    password: str
+
+    @pydantic.validator('email')
+    def validate_email(cls, value):
+        if not('@' in value):
+            raise ValueError('Enter correct email')
+        if not('.' in value):
+            raise ValueError('Enter correct email')
+        return value
+
+    @pydantic.validator('password')
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError('Password short')
+        return value
+
+class PatchUser(pydantic.BaseModel):
+    """
+    Валидация полей при редактировании записи в модели User
+    """
+    email: Optional[str]
+    password: Optional[str]
+
+    @pydantic.validator('email')
+    def validate_email(cls, value):
+        if not ('@' in value):
+            raise ValueError('Enter correct email')
+        if not ('.' in value):
+            raise ValueError('Enter correct email')
+        return value
+
+    @pydantic.validator('password')
+    def validate_password(cls, value):
+        if len(value) < 8:
+            raise ValueError('Password short')
+        return value
